5a4840110c74a1654531725e93a130b4079f0443
Pedro Pablo Correa <pbcorrea@uc.cl>
2021-01-11 16:18:14 -0300

    ADD: Compilation at setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+from setuptools import setup, Extension
+import pybind11
 from distutils.core import setup
-setup (name = 'pyds_metadata_patch',
+
+ext_modules = [
+       Extension(
+       'pyds_tracker_meta',
+       sources = ['pyds_tracker_meta.cpp'],
+       include_dirs=[pybind11.get_include(), '/opt/nvidia/deepstream/deepstream-5.0/sources/includes/', '/usr/include/gstreamer-1.0', '/usr/include/orc-0.4', '/usr/include/gstreamer-1.0', '/usr/include/glib-2.0', '/usr/lib/aarch64-linux-gnu/glib-2.0/include'],
+       language='c++'
+       ),
+       Extension(
+       'pyds_bbox_meta',
+       sources = ['pyds_bbox_meta.cpp'],
+       include_dirs=[pybind11.get_include(), '/opt/nvidia/deepstream/deepstream-5.0/sources/includes/', '/usr/include/gstreamer-1.0', '/usr/include/orc-0.4', '/usr/include/gstreamer-1.0', '/usr/include/glib-2.0', '/usr/lib/aarch64-linux-gnu/glib-2.0/include'],
+       language='c++'
+       ),
+]
+
+setup(
+       name = 'pyds_metadata_patch',
        version = '1.0',
        description = """Install precompiled DeepStream Python bindings for tracker metadata extension""",
        packages=[''],
        package_data={'': ['pyds_tracker_meta.so', 'pyds_bbox_meta.so']},
+       ext_modules=ext_modules
        )
