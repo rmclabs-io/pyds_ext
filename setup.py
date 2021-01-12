19b9fa9f239c831aeceb17aa2e6bb0dedc49e679
Pablo Woolvett <pablowoolvett@gmail.com>
2021-01-12 15:21:11 -0300

    FIX: autosetup and verbose error
    Signed-off-by: Pablo Woolvett <pablowoolvett@gmail.com>

--- setup.py
+++ setup.py
@@ -1,30 +1,54 @@
-from setuptools import setup, Extension
-import pybind11
-from distutils.core import setup
 
-ext_modules = [
-       Extension(
-       'pyds_tracker_meta',
-       sources = ['pyds_tracker_meta.cpp'],
-       include_dirs=[pybind11.get_include(), '/opt/nvidia/deepstream/deepstream-5.0/sources/includes/', '/usr/include/gstreamer-1.0', '/usr/include/orc-0.4', '/usr/include/gstreamer-1.0', '/usr/include/glib-2.0', '/usr/lib/aarch64-linux-gnu/glib-2.0/include'],
-       language='c++'
-       ),
-       Extension(
-       'pyds_bbox_meta',
-       sources = ['pyds_bbox_meta.cpp'],
-       include_dirs=[pybind11.get_include(), '/opt/nvidia/deepstream/deepstream-5.0/sources/includes/', '/usr/include/gstreamer-1.0', '/usr/include/orc-0.4', '/usr/include/gstreamer-1.0', '/usr/include/glib-2.0', '/usr/lib/aarch64-linux-gnu/glib-2.0/include'],
-       language='c++'
-       ),
+import os
+from pathlib import Path
+from setuptools import setup
+import shlex
+import subprocess as sp
+import sys
+import warnings
+try:
+    # Available at setup time due to pyproject.toml
+    from pybind11.setup_helpers import Pybind11Extension
+    from pybind11.setup_helpers import build_ext
+    from pybind11 import get_cmake_dir
+except ImportError as exc:
+    warnings.warn(repr(exc))
+    warnings.warn(f"Maybe you forgot to upgrade pip?")
+    sys.exit(42)
+
+
+INCLUDE_DIRS = [
+    f"{os.environ['DS']}/sources/includes/",
+    *[
+        inc.lstrip('-I')
+        for inc in shlex.split(
+            sp.check_output(
+                shlex.split(
+                    "pkg-config --cflags gstreamer-1.0 gstreamer-video-1.0"
+                )
+            ).decode("utf8")
+        )
+        if inc.startswith("-I")
+    ]
+]
+
+EXT_MODULES = [
+    Pybind11Extension(
+       module.stem,
+       sources = [str(module)],
+       include_pybind11=True,
+       include_dirs=INCLUDE_DIRS,
+    ) for module in Path("src").glob("*.cpp")
 ]
 
 setup(
-       name = 'pyds_metadata_patch',
-       version = '1.0',
-       description = """Install precompiled DeepStream Python bindings for tracker metadata extension""",
-       packages=[''],
-       package_data={'': ['pyds_tracker_meta.so', 'pyds_bbox_meta.so']},
-       ext_modules=ext_modules,
-       setup_requires=[
-        "pybind11 >= 2.6.1",
-    ]
-       )
+    name = 'pyds_metadata_patch',
+    version = '1.0',
+    description = """Install precompiled DeepStream Python bindings for tracker metadata extension""",
+    packages=[''],
+    # package_data={'': Path(".").glob("*.so")},
+    ext_modules=EXT_MODULES,
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    cmdclass={"build_ext": build_ext},
+)
