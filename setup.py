585b4a7481a9131328744c780be2418b2df58dc8
pwoolvett <pablowoolvett@gmail.com>
2021-01-14 15:06:34 -0300

    Fix/setup (#2)
    ADD: bundle precompiled pyds
    
    Signed-off-by: Pablo Woolvett <pablowoolvett@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@
 import os
 from pathlib import Path
 from setuptools import setup
+from setuptools import Extension as BaseExtension
 import shlex
+import shutil
 import subprocess as sp
 import sys
 import warnings
@@ -10,11 +12,12 @@ try:
     # Available at setup time due to pyproject.toml
     from pybind11.setup_helpers import Pybind11Extension
     from pybind11.setup_helpers import build_ext
-    from pybind11 import get_cmake_dir
+    # from pybind11 import get_cmake_dir
 except ImportError as exc:
     warnings.warn(repr(exc))
     warnings.warn(f"Maybe you forgot to upgrade pip?")
     sys.exit(42)
+
 
 
 INCLUDE_DIRS = [
@@ -32,23 +35,57 @@ INCLUDE_DIRS = [
     ]
 ]
 
+
+class CustomExtBuilder(build_ext):
+
+    def build_extension(self, ext):
+        if isinstance(ext, Precompiled):
+            return ext.copy_precompiled(self)
+        return super().build_extension(ext)
+
+
+class Precompiled(BaseExtension):
+
+    def __init__(self, name, precompiled, *args, **kwargs):
+        super().__init__(name, [], *args, **kwargs)
+        self.precompiled = Path(precompiled)
+
+    def copy_precompiled(self, builder):
+        if self.precompiled.exists():
+            path = Path(builder.get_ext_fullpath(self.name))
+            path.parent.mkdir(parents=True)
+            shutil.copyfile(
+                str(self.precompiled),
+                builder.get_ext_fullpath(self.name)
+            )
+        else:
+            print(f"Error: specified file {self.precompiled} not found!", file=sys.stderr)
+
 EXT_MODULES = [
-    Pybind11Extension(
-       module.stem,
-       sources = [str(module)],
-       include_pybind11=True,
-       include_dirs=INCLUDE_DIRS,
-    ) for module in Path("src").glob("*.cpp")
+    Precompiled(
+        "pyds",
+        precompiled="/opt/nvidia/deepstream/deepstream/lib/pyds.so"
+    ),
+    *[
+        Pybind11Extension(
+            module.stem,
+            sources = [str(module)],
+            include_pybind11=True,
+            include_dirs=INCLUDE_DIRS,
+        )
+        for module in Path("src").glob("*.cpp")
+    ]
 ]
 
-setup(
-    name = 'pyds_metadata_patch',
-    version = '1.0.1',
-    description = """DeepStream bindings for tracker and bbox metadata""",
-    packages=[''],
-    # package_data={'': Path(".").glob("*.so")},
-    ext_modules=EXT_MODULES,
-    # Currently, build_ext only provides an optional "highest supported C++
-    # level" feature, but in the future it may provide more features.
-    cmdclass={"build_ext": build_ext},
-)
+def main():
+
+    setup(
+        name = 'pyds_metadata_patch',
+        version = '1.1.2',
+        description = """DeepStream bindings for tracker and bbox metadata""",
+        ext_modules=EXT_MODULES,
+        cmdclass={"build_ext": CustomExtBuilder},
+    )
+
+if __name__ == '__main__':
+    main()
