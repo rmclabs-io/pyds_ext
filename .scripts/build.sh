b82d19c35fcb754e48ab6d1cf64ad61bcd9befa3
Pablo Woolvett <pablowoolvett@gmail.com>
2021-02-17 11:07:48 -0300

    build(script): fix aptitude installs
    Signed-off-by: Pablo Woolvett <pablowoolvett@gmail.com>

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 apt-get update \
   && apt-get install -y \
-      build-essential
+      build-essential \
       python3-venv \
-      libgstreamer-plugins-base1.0-dev
+      libgstreamer-plugins-base1.0-dev \
       python3-dev
 
 python3 -m venv .dockervenv
